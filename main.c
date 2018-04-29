db93e0e72d93954dedee5a9f0f742ec183abc160
DenisLapi <denislapidenis@gmail.com>
2018-04-29 23:58:41 +0300

    Created function for showing graph matrix

--- main.c
+++ main.c
@@ -36,3 +36,20 @@ void create_graph(int *graph_matrix, int num_of_nodes) {
         }
     }
 }
+
+void show_graph_matrix(int *graph_matrix, int num_of_nodes) {
+
+    int row;
+    int col;
+    int *graph_matrix_init = graph_matrix;
+
+    printf("\n\n|----------- Graph Matrix -----------|\n\n");
+    for(row = 0; row < num_of_nodes; row++) {
+        for(col = 0; col < num_of_nodes; col++) {
+            printf("%d ", *graph_matrix_init);
+            graph_matrix_init++;
+        }
+        printf("\n");
+    }
+    printf("\n|------------------------------------|\n\n");
+}
