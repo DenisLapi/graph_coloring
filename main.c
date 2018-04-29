8d20cc579b3641d9ae8c88925ecb2f74159d9428
DenisLapi <denislapidenis@gmail.com>
2018-04-29 23:44:23 +0300

    Allocate memory for graph matrix

--- main.c
+++ main.c
@@ -4,9 +4,15 @@
 int main()
 {
     int num_of_nodes = 0;
+    int *graph_matrix;
 
+    /// Get how much number we will have in our graph
     printf("Enter the number of nodes: ");
     scanf("%d", &num_of_nodes);
+
+    /// Allocate the memory for graph matrix with N rows and N columns
+    graph_matrix = (int*)calloc(num_of_nodes * num_of_nodes, sizeof(int));
+
 
 
     return 0;
